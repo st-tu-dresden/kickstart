bfaab8201e15a34b4999f633ec33cb57287138a5
Oliver Gierke <info@olivergierke.de>
2014-08-15 13:03:09 +0200

    #1 - Extracted kickstart project from Salespoint repository.
    Upgraded dependencies to the latest versions. Simplified setup a little.

---
+++ src/main/java/kickstart/Application.java
@@ -0,0 +1,34 @@
+package kickstart;
+
+import org.salespointframework.SalespointSecurityConfiguration;
+import org.salespointframework.SalespointWebConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@Import({ SalespointWebConfiguration.class })
+@ComponentScan
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin().loginProcessingUrl("/login").and()
+					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+		}
+	}
+}
