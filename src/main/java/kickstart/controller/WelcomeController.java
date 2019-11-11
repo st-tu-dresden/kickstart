596ccecd3b373af24f9b4dfb0ece2145550569e8
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2019-11-11 15:24:58 +0100

    #63 - Specify HTTP method in request mapping.
    The default is to respond to all methods, which can lead to security
    issues as pointed out by SonarQube rule S3752.

--- src/main/java/kickstart/controller/WelcomeController.java
+++ src/main/java/kickstart/controller/WelcomeController.java
@@ -16,12 +16,12 @@
 package kickstart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "welcome";
 	}
