6308667487cac9441338c3e0decedf586994d9fd
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2021-11-16 15:46:02 +0100

    #83 - Switch from technical to functional package names.
    The previous package name `controller` was not a good choice because it
    made it look like that all controller classes of the different
    functional slices should reside in one package, i.e., use a technical
    separation (vs. functional separation).

--- src/main/java/kickstart/welcome/WelcomeController.java
+++ src/main/java/kickstart/welcome/WelcomeController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kickstart.controller;
+package kickstart.welcome;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/kickstart/welcome/WelcomeControllerIntegrationTests.java
+++ src/test/java/kickstart/welcome/WelcomeControllerIntegrationTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kickstart.controller;
+package kickstart.welcome;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
